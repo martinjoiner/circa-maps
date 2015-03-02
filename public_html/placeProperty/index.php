e795648c796aca3882f02b28953c23c626f7c3fa
Martin Joiner <martin@martinjoiner.co.uk>
2015-03-02 21:06:25 +0000

    Ported placeProperty function to child class of MapSection

--- public_html/placeProperty/index.php
+++ public_html/placeProperty/index.php
@@ -4,8 +4,8 @@ header('Content-Type: application/json');
 
 include($_SERVER['DOCUMENT_ROOT'] . '/autoloadRegister.inc.php');
 
-$objMapSection = new MapSection( $_GET['mapID'], $_GET['x'], $_GET['y'] );
+$objPropertyPlacer = new PropertyPlacer( $_GET['mapID'], $_GET['x'], $_GET['y'] );
 
-$arrResult = $objMapSection->placeProperty( $_GET['x'], $_GET['y'] );
+$arrResult = $objPropertyPlacer->placeProperty( $_GET['x'], $_GET['y'] );
 
 echo json_encode($arrResult);
