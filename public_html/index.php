6fea4ae9201010dd88c94b37f7bdf648d4021080
Martin Joiner <martin@martinjoiner.co.uk>
2017-10-12 21:29:08 +0100

    Added integrity attribute to jQuery CDN reference

--- public_html/index.php
+++ public_html/index.php
@@ -132,7 +132,9 @@
 
 	</form>
 
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.js"></script>
+	<script src="http://code.jquery.com/jquery-2.2.4.min.js"
+			integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44="
+			crossorigin="anonymous"></script>
 	<script src="/js/circa.js"></script>
 
 </body>
