126fefdd3c2d34794bf230e106a890b40dc0cb41
Martin Joiner <martin@martinjoiner.co.uk>
2017-10-15 20:05:18 +0100

    Fixed routeSegmentsWithinRange feature that was broken in refactor

--- public_html/api/routeSegmentsWithinRange/index.php
+++ public_html/api/routeSegmentsWithinRange/index.php
@@ -1,13 +1,17 @@
 <?php
 
+if( $_SERVER['REQUEST_METHOD'] !== 'GET' ){
+    throw new exception('Invalid Method', 405);
+}
+
+require $_SERVER['DOCUMENT_ROOT'] . '/../vendor/autoload.php';
+
+$mapSection = new App\MapSection( $_GET['mapID'], $_GET['x'], $_GET['y'] );
+
+$point = new App\Point( $_GET['x'], $_GET['y'] );
+
+$arrResult = $mapSection->getRouteSegmentsWithinRange( $point );
+
 header('Content-Type: application/json');
-
-include($_SERVER['DOCUMENT_ROOT'] . '/autoloadRegister.inc.php');
-
-$objMap = new MapSection( $_GET['mapID'], $_GET['x'], $_GET['y'] );
-
-$point = new Point( $_GET['x'], $_GET['y'] );
-
-$arrResult = $objMap->getRouteSegmentsWithinRange( $point );
 
 echo json_encode($arrResult);
